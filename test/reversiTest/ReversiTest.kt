a51e2e38d88f8aba07f6c0bb0f38009227c51312
DiscreetDmitriy <dimacaesar@gmail.com>
2019-04-07 20:32:16 +0300

    Created Tests

---
+++ test/reversiTest/ReversiTest.kt
@@ -0,0 +1,12 @@
+package reversiTest
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.*
+
+class ReversiTest {
+    @Test
+    fun testOfSomething() {
+        assertEquals(true,true)
+    }
+
+}
