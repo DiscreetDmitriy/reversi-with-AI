ff2cc29c1e76763243ce2867f5b3430e9c755a41
Dminty <dimacaesar@gmail.com>
2019-12-22 02:05:26 +0300

    AI algorithm placeholders

---
+++ src/model/ai/MiniMax.kt
@@ -0,0 +1,20 @@
+package model.ai
+
+import model.Chip
+
+class MiniMax {
+    companion object {
+        fun solve(
+            board: List<List<Chip>>,
+            chip: Chip,
+            depth: Int,
+            evaluator: Evaluator
+        ): Pair<Int, Int> {
+            var bestMove = -1 to -1;
+
+
+
+            return bestMove
+        }
+    }
+}
