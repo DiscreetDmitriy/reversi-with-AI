e3528ffa7bce0d2c5ec308bf4476b2e836ff110c
Dminty <dimacaesar@gmail.com>
2019-12-30 12:11:57 +0300

    minor fixes

--- src/model/ai/MiniMax.kt
+++ src/model/ai/MiniMax.kt
@@ -2,15 +2,12 @@ package model.ai
 
 import model.Field
 
+@Suppress("NAME_SHADOWING")
+
 class MiniMax {
     companion object {
-        private var nodesExplored = 0
 
-        fun solve(
-            field: Field,
-            depth: Int,
-            eval: Evaluator
-        ): Pair<Int, Int> {
+        fun solve(field: Field, depth: Int, eval: Evaluator): Pair<Int, Int> {
             val field1 = copyField(field)
 
             var bestMove = -1 to -1
@@ -33,19 +30,7 @@ class MiniMax {
                     bestMove = move
                 }
             }
-            println("Nodes explored: $nodesExplored")
             return bestMove
-        }
-
-        private fun copyField(field: Field): Field {
-            val field1 = Field()
-
-            for (i in field1.board.indices) {
-                for (j in field1.board[i].indices)
-                    field1.board[i][j] = field.board[i][j]
-            }
-            field1.humanTurn = field.humanTurn
-            return field1
         }
 
         private fun minmax(
@@ -56,8 +41,6 @@ class MiniMax {
             alpha: Int,
             beta: Int
         ): Int {
-            nodesExplored++
-
             var alpha = alpha
             var beta = beta
 
@@ -74,13 +57,21 @@ class MiniMax {
                 score = Int.MIN_VALUE
                 for (move in field.occupiable(field.playerAI)) {
                     val field = copyField(field)
-                    val childScore = minmax(field, depth - 1, eval, false, alpha, beta)
+                    val childScore = minmax(
+                        field = field,
+                        depth = depth - 1,
+                        eval = eval,
+                        max = false,
+                        alpha = alpha,
+                        beta = beta
+                    )
 
                     if (childScore > score)
                         score = childScore
 
                     if (score > alpha)
                         alpha = score
+
                     if (beta <= alpha)
                         break
                 }
@@ -90,17 +81,37 @@ class MiniMax {
                 for (move in field.occupiable(field.playerHuman)) {
                     val field = copyField(field)
                     field.makeTurn(move.first, move.second, field.playerAI)
-                    val childScore = minmax(field, depth - 1, eval, true, alpha, beta)
+                    val childScore = minmax(
+                        field = field,
+                        depth = depth - 1,
+                        eval = eval,
+                        max = true,
+                        alpha = alpha,
+                        beta = beta
+                    )
                     if (childScore < score)
                         score = childScore
+
                     if (score < beta)
                         beta = score
+
                     if (beta <= alpha)
                         break
                 }
             }
 
             return score
+        }
+
+        private fun copyField(field: Field): Field {
+            val field1 = Field()
+
+            for (i in field1.board.indices) {
+                for (j in field1.board[i].indices)
+                    field1.board[i][j] = field.board[i][j]
+            }
+            field1.humanTurn = field.humanTurn
+            return field1
         }
     }
 }
